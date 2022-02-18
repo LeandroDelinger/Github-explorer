8ed9e338d159ee03844d559f171d8751c66ee5ae
LeandroDelinger <leandro.delinger@gmail.com>
2022-02-17 23:17:55 -0300

    explorando repositórios no github

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles/global.scss";
+import { RepositoryList } from "./components/RepositoryList";
+
+export function App() {
+  return (
+    <>
+      <RepositoryList />
+    </>
+  );
+}
